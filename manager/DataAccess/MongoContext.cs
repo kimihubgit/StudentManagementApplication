965e15c0196f56d9f5e93b848b7c6c52caf324cf
KimiGit <vkhai2k5@gmail.com>
2026-04-03 23:59:30 +0700

    feat: tạo class kết nối với database tổng

---
+++ manager/DataAccess/MongoContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using MongoDB.Driver;
+using System;
+using System.Configuration;
+
+namespace manager.DataAccess
+{
+    public class MongoContext
+    {
+        private readonly IMongoDatabase _database;
+        public MongoContext()
+        {
+            try
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["MongoDbConnection"].ConnectionString;
+                string databaseName = ConfigurationManager.AppSettings["DatabaseName"];
+                var client = new MongoClient(connectionString);
+                _database = client.GetDatabase(databaseName);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Không thể kết nối đến MongoDB: " + ex.Message);
+            }
+        }
+        public IMongoCollection<T> GetCollection<T>(string collectionName)
+        {
+            return _database.GetCollection<T>(collectionName);
+        }
+    }
+}
