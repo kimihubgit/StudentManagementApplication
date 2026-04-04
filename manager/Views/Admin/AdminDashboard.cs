98544667974cb79b53511cfe1df501ea779193b7
KimiGit <vkhai2k5@gmail.com>
2026-04-04 18:59:45 +0700

    [ui]: thêm giao diện admin

---
+++ manager/Views/Admin/AdminDashboard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace manager.Views.Admin
+{
+    public partial class AdminDashboard : Form
+    {
+        public AdminDashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
