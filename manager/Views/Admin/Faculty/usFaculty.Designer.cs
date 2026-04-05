a36512aefc236b1f6358ac100da20f827067c248
KimiGit <vkhai2k5@gmail.com>
2026-04-05 11:42:14 +0700

    [fix]: thêm tính năng thêm sữa xóa cho faculty

--- manager/Views/Admin/Faculty/usFaculty.Designer.cs
+++ manager/Views/Admin/Faculty/usFaculty.Designer.cs
@@ -48,54 +48,64 @@
             // 
             // button1
             // 
-            button1.Location = new Point(34, 39);
+            button1.Location = new Point(30, 29);
+            button1.Margin = new Padding(3, 2, 3, 2);
             button1.Name = "button1";
-            button1.Size = new Size(130, 50);
+            button1.Size = new Size(114, 38);
             button1.TabIndex = 0;
             button1.Text = "Thêm";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += btnThem_Click;
             // 
             // button2
             // 
-            button2.Location = new Point(180, 39);
+            button2.Location = new Point(158, 29);
+            button2.Margin = new Padding(3, 2, 3, 2);
             button2.Name = "button2";
-            button2.Size = new Size(130, 50);
+            button2.Size = new Size(114, 38);
             button2.TabIndex = 1;
             button2.Text = "Sửa";
             button2.UseVisualStyleBackColor = true;
+            button2.Click += btnSua_Click;
             // 
             // button3
             // 
-            button3.Location = new Point(325, 39);
+            button3.Location = new Point(284, 29);
+            button3.Margin = new Padding(3, 2, 3, 2);
             button3.Name = "button3";
-            button3.Size = new Size(130, 50);
+            button3.Size = new Size(114, 38);
             button3.TabIndex = 2;
             button3.Text = "Xóa";
             button3.UseVisualStyleBackColor = true;
+            button3.Click += btnXoa_Click;
             // 
             // button4
             // 
-            button4.Location = new Point(481, 39);
+            button4.Location = new Point(421, 29);
+            button4.Margin = new Padding(3, 2, 3, 2);
             button4.Name = "button4";
-            button4.Size = new Size(130, 50);
+            button4.Size = new Size(114, 38);
             button4.TabIndex = 3;
             button4.Text = "Làm Mới";
             button4.UseVisualStyleBackColor = true;
+            button4.Click += btnLamMoi_Click;
             // 
             // button5
             // 
-            button5.Location = new Point(762, 39);
+            button5.Location = new Point(667, 29);
+            button5.Margin = new Padding(3, 2, 3, 2);
             button5.Name = "button5";
-            button5.Size = new Size(130, 50);
+            button5.Size = new Size(114, 38);
             button5.TabIndex = 4;
             button5.Text = "Import";
             button5.UseVisualStyleBackColor = true;
             // 
             // button6
             // 
-            button6.Location = new Point(920, 39);
+            button6.Location = new Point(805, 29);
+            button6.Margin = new Padding(3, 2, 3, 2);
             button6.Name = "button6";
-            button6.Size = new Size(130, 50);
+            button6.Size = new Size(114, 38);
             button6.TabIndex = 5;
             button6.Text = "Export";
             button6.UseVisualStyleBackColor = true;
@@ -108,66 +118,72 @@
             // 
             // txtMaKhoa
             // 
-            txtMaKhoa.Location = new Point(34, 137);
+            txtMaKhoa.Location = new Point(30, 103);
+            txtMaKhoa.Margin = new Padding(3, 2, 3, 2);
             txtMaKhoa.Name = "txtMaKhoa";
-            txtMaKhoa.Size = new Size(252, 27);
+            txtMaKhoa.Size = new Size(221, 23);
             txtMaKhoa.TabIndex = 7;
             txtMaKhoa.TextChanged += textBox1_TextChanged;
             // 
             // txtTenKhoa
             // 
-            txtTenKhoa.Location = new Point(325, 137);
+            txtTenKhoa.Location = new Point(284, 103);
+            txtTenKhoa.Margin = new Padding(3, 2, 3, 2);
             txtTenKhoa.Name = "txtTenKhoa";
-            txtTenKhoa.Size = new Size(252, 27);
+            txtTenKhoa.Size = new Size(221, 23);
             txtTenKhoa.TabIndex = 8;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(44, 114);
+            label1.Location = new Point(38, 86);
             label1.Name = "label1";
-            label1.Size = new Size(72, 20);
+            label1.Size = new Size(57, 15);
             label1.TabIndex = 9;
             label1.Text = "Mã Khoa ";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(341, 114);
+            label2.Location = new Point(298, 86);
             label2.Name = "label2";
-            label2.Size = new Size(70, 20);
+            label2.Size = new Size(56, 15);
             label2.TabIndex = 10;
             label2.Text = "Tên Khoa";
             // 
             // dtpNgayThanhLap
             // 
-            dtpNgayThanhLap.Location = new Point(662, 137);
+            dtpNgayThanhLap.Location = new Point(579, 103);
+            dtpNgayThanhLap.Margin = new Padding(3, 2, 3, 2);
             dtpNgayThanhLap.Name = "dtpNgayThanhLap";
-            dtpNgayThanhLap.Size = new Size(308, 27);
+            dtpNgayThanhLap.Size = new Size(270, 23);
             dtpNgayThanhLap.TabIndex = 11;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(662, 114);
+            label3.Location = new Point(579, 86);
             label3.Name = "label3";
-            label3.Size = new Size(120, 20);
+            label3.Size = new Size(97, 15);
             label3.TabIndex = 12;
             label3.Text = "Ngày Thành Lập ";
             // 
             // dgvKhoa
             // 
+            dgvKhoa.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             dgvKhoa.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvKhoa.Location = new Point(34, 202);
+            dgvKhoa.Location = new Point(30, 152);
+            dgvKhoa.Margin = new Padding(3, 2, 3, 2);
             dgvKhoa.Name = "dgvKhoa";
             dgvKhoa.RowHeadersWidth = 51;
-            dgvKhoa.Size = new Size(1016, 465);
+            dgvKhoa.Size = new Size(889, 349);
             dgvKhoa.TabIndex = 13;
             // 
             // usFaculty
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
             Controls.Add(dgvKhoa);
             Controls.Add(label3);
             Controls.Add(dtpNgayThanhLap);
@@ -181,8 +197,9 @@
             Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "usFaculty";
-            Size = new Size(1073, 705);
+            Size = new Size(939, 529);
             ((System.ComponentModel.ISupportInitialize)dgvKhoa).EndInit();
             ResumeLayout(false);
             PerformLayout();
