ecf6cf5967b14e43a4391d04945d820768278f72
KimiGit <vkhai2k5@gmail.com>
2026-04-07 18:25:57 +0700

    [fix]: sữa các nút trên giao diện v2

--- manager/Models/ClassRoom.cs
+++ manager/Models/ClassRoom.cs
@@ -10,16 +10,12 @@ namespace Manager_Student.Models
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; } // Giữ nguyên cái này vì đây là khóa chính của MongoDB
+        public string Id { get; set; }
 
         public string ClassCode { get; set; }
         public string ClassName { get; set; }
         public int AcademicYear { get; set; }
-
-        // XÓA DÒNG [BsonRepresentation...] Ở ĐÂY
         public string MajorId { get; set; }
-
-        // XÓA DÒNG [BsonRepresentation...] Ở ĐÂY
         public string HomeroomTeacherId { get; set; }
     }
 }
